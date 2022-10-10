83d01e24d32618247b7afea42e35ebee1e7e2a1e
Mhmmdfhkri <90515626+Mhmmdfhkri@users.noreply.github.com>
2022-10-10 04:01:47 -0700

    update

--- DDL, DML Dan DCL.sql
+++ DDL, DML Dan DCL.sql
@@ -96,7 +96,7 @@ create user dream@localhost identified by 'tama123';
 -- memberikan semua akses tabel kepada user 
 grant all privileges on designdatabase to 'admin'@'localhost';
 
--- memberi sebagian hak akses tabel kepada user 
+-- memberi hak akses tabel kepada user 
 grant all privileges on designdatabase.students to 'admin'@'localhost';
 grant all privileges on  designdatabase.course to 'upstream'@'localhost';
 grant all privileges on designdatabase.schedule to 'upstream'@'localhost'; 
